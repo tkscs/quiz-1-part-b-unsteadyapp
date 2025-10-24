7c53e0312b9bec748e9663b23b2cb7b964a207f1
unsteadyapp <akanodia2029@kehillahstudent.org>
2025-10-24 11:29:50 -0700

    Finished

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import turtle
-
+import math
 # YOUR CODE HERE
+for i in range(4):
+    turtle.forward(100)
+    turtle.right(90)
+turtle.forward(50)
+turtle.right(45)
+for g in range(4):
+    turtle.forward(math.sqrt(50**2+50**2))
+    turtle.right(90)
+
+
 
 turtle.exitonclick()
